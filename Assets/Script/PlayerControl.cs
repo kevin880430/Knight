ad9a6930476c4dc60e2f42e4ccd20cf438899e6f
kevin880430 <kevin880430@gmail.com>
2023-10-02 02:18:06 +0900

    Initial commit

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private AttackState attackState = AttackState.Idle;
+    private float lastAttackTime;
+    public float comboTimeout = 1.0f; // 定义连击的超时时间
+    public float attackCooldown = 0.5f; // 攻击冷却时间
+    public enum AttackState
+    {
+        Idle,        // 空闲状态
+        FirstAttack, // 第一段攻击
+        SecondAttack // 第二段攻击
+    }
+    private Animator PlayerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerAnimator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        // 检查玩家输入或点击以触发攻击
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Attcak();
+        }
+
+        // 如果攻击超时，重置为Idle状态
+        if (attackState != AttackState.Idle && Time.time - lastAttackTime > comboTimeout)
+        {
+            attackState = AttackState.Idle;
+        }
+    }
+    private void Attcak()
+    {
+        // 根据当前攻击状态执行不同的逻辑
+        switch (attackState)
+        {
+            case AttackState.Idle:
+                // 触发第一段攻击
+                FirstAttack();
+                break;
+            case AttackState.FirstAttack:
+                // 触发第二段攻击
+                SecondAttack();
+                break;
+            case AttackState.SecondAttack:
+                // 重置为第一段攻击
+                FirstAttack();
+                break;
+        }
+    }
+    private void FirstAttack()
+    {
+        if (Time.time - lastAttackTime >= attackCooldown)
+        {
+            // 执行第一段攻击逻辑
+            Debug.Log("Performing First Attack");
+            attackState = AttackState.FirstAttack;
+            PlayerAnimator.SetTrigger("Attack1");
+            lastAttackTime = Time.time;
+        }
+      
+    }
+
+    private void SecondAttack()
+    {
+        // 执行第二段攻击逻辑
+        Debug.Log("Performing Second Attack");
+        attackState = AttackState.SecondAttack;
+        PlayerAnimator.SetTrigger("Attack2");
+        lastAttackTime = Time.time;
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // 移动速度
+    public float jumpForce = 7f; // 跳跃力量
+    public LayerMask groundLayer; // 地面层
+
+    private SpriteRenderer sr;
+    private Rigidbody2D rb2d;
+    public bool isGrounded;
+    private Animator PlayerAnimator;
+
+    void Start()
+    {
+        PlayerAnimator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // 检测是否在地面上
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+        isGrounded = hit.collider != null;
+        Debug.DrawRay(transform.position, Vector2.down * 1f, Color.red);
+        Move();
+
+        // 跳跃
+        if (Input.GetKeyDown(KeyCode.C) && isGrounded)
+        {
+            Jump();
+        }
+
+        // 处理下落
+        if (rb2d.velocity.y<0)
+        {
+            Fall();
+        }
+    }
+
+    void Jump()
+    {
+        // 施加向上的力以进行跳跃
+        rb2d.AddForce(Vector2.up * jumpForce);
+        if (rb2d.velocity.y >= 0)
+        {
+            PlayerAnimator.SetBool("Jump",true);
+            PlayerAnimator.SetBool("Fall", false);
+            PlayerAnimator.SetBool("JumpFallBtw", false);
+
+        }
+        if (rb2d.velocity.y == 0&&(PlayerAnimator.GetBool("Jump")||(PlayerAnimator.GetBool("Fall"))))
+        {
+            PlayerAnimator.SetBool("JumpFallBtw", true);
+        }
+        if(rb2d.velocity.y<0)
+        {
+            PlayerAnimator.SetBool("Fall", true);
+            PlayerAnimator.SetBool("Jump", false);
+            PlayerAnimator.SetBool("JumpFallBtw", false);
+        }
+    }
+    void Move()
+    {
+        // 左右移动
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(horizontalInput * moveSpeed, rb2d.velocity.y);
+        if (horizontalInput != 0&&isGrounded)
+        {
+            PlayerAnimator.SetBool("Run", true);
+        }
+        else
+        {
+            PlayerAnimator.SetBool("Run", false);
+        }
+    }
+    void Fall()
+    {
+        
+    }
+}
