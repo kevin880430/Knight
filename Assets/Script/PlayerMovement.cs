c8aa042ac0a9d325f2c11d8ebdadf10484c35b1c
kevin880430 <kevin880430@gmail.com>
2023-10-03 13:19:43 +0900

    animation arrange
    jump,falling attack arranger 1st ver

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,11 +5,20 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f; // 移动速度
     public float jumpForce = 7f; // 跳跃力量
     public LayerMask groundLayer; // 地面层
+    float JumpTimer;
 
     private SpriteRenderer sr;
     private Rigidbody2D rb2d;
     public bool isGrounded;
     private Animator PlayerAnimator;
+    private FallingState fallingstate;
+    public bool isJumptoFall=false;
+    public bool isFalling=false;
+    public enum FallingState
+    {
+        JumptoFall,
+        ElsetoFall
+    }
 
     void Start()
     {
@@ -21,62 +30,81 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // 检测是否在地面上
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.9f, groundLayer);
         isGrounded = hit.collider != null;
-        Debug.DrawRay(transform.position, Vector2.down * 1f, Color.red);
+        PlayerAnimator.SetBool("IsGrounded", isGrounded);
+        Debug.DrawRay(transform.position, Vector2.down * 0.9f, Color.red);
         Move();
 
         // 跳跃
-        if (Input.GetKeyDown(KeyCode.C) && isGrounded)
-        {
+        
             Jump();
-        }
 
-        // 处理下落
-        if (rb2d.velocity.y<0)
+        if (isGrounded&&JumpTimer>=0.08f)
         {
-            Fall();
+            PlayerAnimator.SetBool("Jump", false);
+            JumpTimer = 0;
         }
+        Fall();
+       
     }
 
     void Jump()
     {
-        // 施加向上的力以进行跳跃
-        rb2d.AddForce(Vector2.up * jumpForce);
-        if (rb2d.velocity.y >= 0)
-        {
-            PlayerAnimator.SetBool("Jump",true);
-            PlayerAnimator.SetBool("Fall", false);
-            PlayerAnimator.SetBool("JumpFallBtw", false);
+        if (Input.GetKeyDown(KeyCode.C))
+        { // 施加向上的力以进行跳跃 
+            if (isGrounded)
+            {
+                rb2d.AddForce(Vector2.up * jumpForce);
+                PlayerAnimator.SetFloat("Run", 0);
+                PlayerAnimator.SetBool("Jump", true);
+                
+            }
 
         }
-        if (rb2d.velocity.y == 0&&(PlayerAnimator.GetBool("Jump")||(PlayerAnimator.GetBool("Fall"))))
+        if (PlayerAnimator.GetBool("Jump"))
         {
-            PlayerAnimator.SetBool("JumpFallBtw", true);
-        }
-        if(rb2d.velocity.y<0)
-        {
-            PlayerAnimator.SetBool("Fall", true);
-            PlayerAnimator.SetBool("Jump", false);
-            PlayerAnimator.SetBool("JumpFallBtw", false);
+            JumpTimer += Time.deltaTime;
         }
     }
+   
     void Move()
     {
         // 左右移动
         float horizontalInput = Input.GetAxis("Horizontal");
         rb2d.velocity = new Vector2(horizontalInput * moveSpeed, rb2d.velocity.y);
-        if (horizontalInput != 0&&isGrounded)
+        PlayerAnimator.SetFloat("Run", Mathf.Abs(horizontalInput));
+        if (horizontalInput > 0)
         {
-            PlayerAnimator.SetBool("Run", true);
+            sr.flipX = false;
         }
-        else
+        if (horizontalInput <0)
         {
-            PlayerAnimator.SetBool("Run", false);
+            sr.flipX = true;
         }
     }
     void Fall()
-    {
-        
+    { 
+        int intValue = (int)Mathf.Floor(rb2d.velocity.y);
+
+        if (!isGrounded)
+        {
+            if (intValue == 0 )
+                isJumptoFall = true;
+            PlayerAnimator.SetBool("JumptoFall", isJumptoFall);
+        }
+        if (isGrounded)
+        {
+            isFalling = false;
+            PlayerAnimator.SetBool("Fall", isFalling);
+        }
+        if (rb2d.velocity.y<0 && !isGrounded) 
+        {
+            isFalling = true;
+            isJumptoFall = false;
+            PlayerAnimator.SetBool("Fall", isFalling);
+
+        }
     }
+  
 }
